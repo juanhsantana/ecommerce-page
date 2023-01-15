0bd4c94d9fea1f0599262f955dbe6594edb5752e
Juan <contatojuanhenrique@gmail.com>
2023-01-15 00:39:32 -0300

    oi

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,108 @@
-import React, { useState } from "react";
-import { Navbar } from "../../components/Navbar/Navbar";
-import { HomeContainer, ProductContainer } from './styles.module'
+import React, { useState } from "react"
+import { Navbar } from "../../components/Navbar/Navbar"
+import Tenis from "/public/nam.jpg"
+
+import { AiOutlineShoppingCart } from "react-icons/ai"
+import { MdVerified } from "react-icons/md"
+import { BsPlusLg } from "react-icons/bs"
+import { FaMinus } from "react-icons/fa"
+import {
+  HomeContainer,
+  ProductContainer,
+  ImageProduct,
+  ProductInformation,
+  Title,
+  Price,
+  ProductNumber,
+  Text,
+  ActionButtons,
+  ProductAmount,
+  AddToCart,
+  ButtonToCart,
+} from "./styles.module"
 
 export const Home = () => {
+  const [amount, setAmount] = useState(0)
+
+  const addAmount = () => {
+    if (amount >= 0) {
+      setAmount(amount + 1)
+    }
+  }
+
+  const subAmount = () => {
+    if (amount > 0) {
+      setAmount(amount - 1)
+    }
+  }
+
   return (
     <>
       <Navbar />
       <HomeContainer>
-        <ProductContainer></ProductContainer>
+        <ProductContainer>
+          <ImageProduct>
+            <img src={Tenis} alt="" />
+          </ImageProduct>
+
+          <ProductInformation>
+            <Title>
+              TÊNIS NIKE AIR MAX PLUS FEMINIO{" "}
+              <MdVerified size={25} color="#1877F2" />
+            </Title>
+            <Price>R$ 1.199,99</Price>
+            <ProductNumber>
+              <Text>Select your size:</Text>
+              <div>
+                <button value={33.3}>33.3</button>
+                <button value={34}>34</button>
+                <button value={34.5}>34.5</button>
+                <button value={35}>35</button>
+                <button value={35.5}>35.5</button>
+                <button value={36}>36</button>
+                <button value={36.5}>36.5</button>
+                <button value={37}>37</button>
+                <button value={37.5}>37.5</button>
+                <button value={38}>38</button>
+                <button value={38.5}>38.5</button>
+                <button value={39}>39</button>
+                <button value={39.5}>39.5</button>
+                <button value={40}>40</button>
+              </div>
+            </ProductNumber>
+          </ProductInformation>
+        </ProductContainer>
+
+        <ActionButtons>
+          <ProductAmount>
+            <FaMinus
+              onClick={subAmount}
+              color="white"
+              size={28}
+              cursor="pointer"
+            />
+            <input
+              className="amount_input"
+              type="text"
+              value={amount}
+              disabled
+            />
+            <BsPlusLg
+              onClick={addAmount}
+              color="white"
+              size={28}
+              cursor="pointer"
+            />
+          </ProductAmount>
+
+          <ButtonToCart>
+            <AddToCart onClick={() => alert('Produto adicionado ao carrinho')}>
+              Adicionar ao carrinho
+              <AiOutlineShoppingCart size={20} />
+            </AddToCart>
+          </ButtonToCart>
+        </ActionButtons>
       </HomeContainer>
     </>
-  );
-};
+  )
+}
--- src/pages/Home/styles.module.tsx
+++ src/pages/Home/styles.module.tsx
@@ -1,5 +1,178 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
-export const HomeContainer = styled.div``
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1100px;
+  width: 100%;
+  gap: 30px;
+  margin: 0 auto;
+  padding: 20px;
+`
 
-export const ProductContainer = styled.div``
+export const ProductContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  gap: 30px;
+
+  @media screen and (max-width: 790px) {
+    flex-direction: column;
+  }
+`
+
+export const ImageProduct = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 50%;
+  width: 100%;
+  height: 100%;
+
+  > img {
+    width: 100%;
+    border-radius: 10px;
+  }
+
+  @media screen and (max-width: 790px) {
+    max-width: 80%;
+  }
+`
+
+export const ProductInformation = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-start;
+  max-width: 50%;
+  width: 100%;
+  height: 400px;
+
+  @media screen and (max-width: 790px) {
+    max-width: 100%;
+    justify-content: center;
+    gap: 50px;
+  }
+`
+
+export const Title = styled.h3`
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+  gap: 10px;
+
+  @media screen and (max-width: 440px) {
+    font-size: 13px;
+    > svg {
+      width: 20px;
+    }
+  }
+`
+
+export const Price = styled.span`
+  font-weight: 500;
+`
+
+export const Text = styled.span`
+  font-weight: 500;
+
+  @media screen and (max-width: 440px) {
+    font-size: 13px;
+  }
+`
+
+export const ProductNumber = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+
+  > div {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 10px;
+
+    > button {
+      border: 1px solid black;
+      border-radius: 10px;
+      padding: 5px 8px;
+      cursor: pointer;
+      background-color: transparent;
+      transition: all 0.2s ease-in-out;
+      :hover {
+        opacity: 70%;
+      }
+    }
+  }
+`
+
+export const ActionButtons = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+
+  @media screen and (max-width: 790px) {
+    flex-direction: column;
+    gap: 50px;
+  }
+`
+
+export const ProductAmount = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 50%;
+  width: 100%;
+  gap: 30px;
+
+  > input {
+    width: 30px;
+    font-size: 15px;
+    border: none;
+    padding-left: 10px;
+    font-weight: 500;
+    color: black;
+    background-color: transparent;
+  }
+
+  > svg {
+    background-color: #ff5400;
+    border-radius: 50%;
+    padding: 7px;
+  }
+
+  @media screen and (max-width: 790px) {
+    max-width: 100%;
+  }
+`
+
+export const ButtonToCart = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 50%;
+  width: 100%;
+
+  @media screen and (max-width: 790px) {
+    max-width: 100%;
+  }
+`
+
+export const AddToCart = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  gap: 10px;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 8px;
+  background-color: #ff5400;
+
+  font-size: 17px;
+  font-weight: 400;
+  color: white;
+  cursor: pointer;
+`
